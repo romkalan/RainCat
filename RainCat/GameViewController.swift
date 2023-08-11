98c88bfff0e0485400130a894d22b3ded2dbf9d6
romkalan <romkalan@mail.ru>
2023-08-11 12:02:47 +0300

    initial settings for project

--- RainCat/GameViewController.swift
+++ RainCat/GameViewController.swift
@@ -14,29 +14,23 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let scene = GameScene(size: view.frame.size)
+
         if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
+          view.presentScene(scene)
+          view.ignoresSiblingOrder = true
+          view.showsPhysics = true
+          view.showsFPS = true
+          view.showsNodeCount = true
         }
     }
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
+        return .landscape
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
 
     override var prefersStatusBarHidden: Bool {
